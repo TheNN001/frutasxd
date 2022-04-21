e1089bd498442ad889f82ed03128ed7f5b1b6744
Sebastian Vargas <sebastian970809@hotmail.com>
2022-04-21 11:56:18 -0500

    Completada la primera version, con nombre y descripcion en hover

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { catchError, EMPTY, tap } from 'rxjs';
+import { Fruta } from 'src/app/frutas/fruta';
 import { FrutasService } from 'src/app/frutas/frutas.service';
 
 @Component({
@@ -8,11 +9,19 @@ import { FrutasService } from 'src/app/frutas/frutas.service';
 })
 export class DashboardComponent implements OnInit {
   errorMessage = '';
+  frutaSel: Fruta[] | undefined
   frutas$ = this.frutasService.frutas$;
-  frutaHover: boolean = false;
+  frutaHoverBool: boolean = false;
+  selectedFrutaNomb : string | undefined;
+  selectedFrutaDesc : string | undefined;
   constructor(private frutasService: FrutasService) { }
 
   ngOnInit(): void {
-    this.frutaHover = false;
+    this.frutaHoverBool = false;
+  }
+
+  frutaHover(frutaId : number) {
+    this.frutaHoverBool = !this.frutaHoverBool;
+    this.frutaSel = this.frutas$.filter(frutaUni => frutaUni.fruta_id === frutaId)
   }
 }
--- src/app/components/fruta/fruta-desc/fruta-desc.component.ts
+++ src/app/components/fruta/fruta-desc/fruta-desc.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Fruta } from 'src/app/frutas/fruta';
 
 @Component({
@@ -6,7 +6,14 @@ import { Fruta } from 'src/app/frutas/fruta';
   templateUrl: './fruta-desc.component.html',
   styleUrls: ['./fruta-desc.component.scss']
 })
-export class FrutaDescComponent {
-  @Input() frutaInfo: Fruta | undefined;
+export class FrutaDescComponent implements OnInit{
+  @Input() frutaInfo: Fruta[] | undefined;
+  frutaName : any;
+  frutaDescription : any;
   constructor() { }
+
+  ngOnInit(): void {
+    this.frutaName = this.frutaInfo?.[0].fruta_name;
+    this.frutaDescription = this.frutaInfo?.[0].fruta_desc;
+  }
 }
